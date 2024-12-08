0c75f7d3f7e566d4731a4762f38d49aeeffdde01
TiO2EvoLve <49835162+TiO2EvoLve@users.noreply.github.com>
2024-12-08 19:28:55 +0800

    2024/12/8

--- code/obj/Debug/touhougame.AssemblyInfo.cs
+++ code/obj/Debug/touhougame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("touhougame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae5fd17bc43736dafad844f0762e38ee5c2f8ba7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+410d414dc4c7b2e0d842af2f83a4284f7353f553")]
 [assembly: System.Reflection.AssemblyProductAttribute("touhougame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("touhougame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
